21d43689c47b896e3f940994361f01e9ce89de74
akiralucas <akiralucas740@gmail.com>
2024-12-05 09:46:30 -0300

    Add files via upload

---
+++ aula02-python.py
@@ -0,0 +1,12 @@
+input('seu nome')
+print(input,'quecoisa')
+nome_usuario = 'lucas' # tipo -> str = string
+input ('qual e a sua idade')# tipo ->int numero interiro
+print (input,'a idade',nome_usuario)
+input('qual e a autura do usario?')
+print(input,' essa e a sua altuar')
+idade_usario='19' 
+autura_usario='1.85'#float 
+print('seja bem vindo',nome_usuario)
+print('seu nome:',nome_usuario,',sua idade é:',idade_usario,',altura e de:',autura_usario)
+
